a22be3020935acaf3ee22af26f8fd9311ab0d442
pratyushsahay <pratyush97@gmail.com>
2019-05-08 01:43:05 -0400

    Created player schema in models/Players.js

---
+++ models/Players.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+
+var playerSchema = new mongoose.Schema({
+    age: {
+        type: Number,
+        min: 0,
+        required: true
+    },
+    teams: [String],
+    championships: {
+        type: Number,
+        min: 0,
+        required = true
+    },
+    retired: {
+        type: String,
+        required: true
+    },
+    height: {
+        type: String,
+        required: true
+    },
+    position: {
+        type: String,
+        required: true
+    },
+    weight: {
+        type: Number,
+        min: 0,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    }
+});
+
+var Player = mongoose.model('Player',playerSchema);
+
+module.exports = Player;
